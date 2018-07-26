8ad67fe07c0396a9897fec0c2c7de9295a6b9ef4
Simon Harris <simonbharris@gmail.com>
2018-07-25 23:08:42 -0700

    Some initial work

---
+++ fillit/fillit.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sharris <sharris@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/25 21:44:48 by sharris           #+#    #+#             */
+/*   Updated: 2018/07/25 21:44:48 by sharris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+#include "../libft/libft.h"
+
+# define VALID_CHAR(x) (x == '#' || x == '.' | x == '\n')
+
+typedef struct	s_shape
+{
+	char **shape;
+	int tsides;
+	int numblk;
+	int len;
+}				t_shape;
+
+#endif
